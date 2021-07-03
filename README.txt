0fbc9abf90b7a200002752bed6ffe4ceedec39a2
avi <avicoomar@gmail.com>
2021-07-03 17:09:39 +0530

    first

---
+++ README.txt
@@ -0,0 +1,21 @@
+1) All the necessary files are present here including
+   ML model, Dataset etc.
+
+2) So, to test everything you'll need to install Flask.
+   To do that, open cmd/terminal and type 
+   "pip install flask"
+
+3) To run the local server type "python aviii.py" in the
+   terminal. Make sure to navigate to the mini project's 
+   folder before running.
+
+4) While the server is running you might encounter some 404 
+   errors in the terminal. Ignore them.
+
+5) To use a different ML model, Delete the .ipynb and .pkl
+   files. Rerun the new model. Make sure that the name of 
+   the pkl file is "testmodel.pkl"
+
+*NOTE : You cannot directly run index.html as the 
+	css,js and other assets wouldn't load.
+	You have to run the server to test the application.*
---
+++ aviii.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+
+# Load ML model
+model = pickle.load(open('testmodel.pkl', 'rb'))
+
+# Create application
+app = Flask(__name__)
+#app = Flask('flaskapp', static_url_path='/avi_assets')
+
+# Bind home function to URL
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# Bind predict function to URL
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Put all form entries values in a list
+    features = [float(i) for i in request.form.values()]
+    print("Datatype of features=",type(features))
+    # Convert features to array
+    array_features = [np.array(features)]
+    # Predict features
+    prediction = model.predict(array_features)
+
+    output = prediction
+
+    # Check the output values and retrive the result with html tag based on the value
+    if output == 1:
+        return render_template('result_no.html',
+                               result='The patient is not likely to have heart disease!')
+    else:
+        return render_template('result_yes.html',
+                               result='The patient is likely to have heart disease!')
+
+
+if __name__ == '__main__':
+    # Run the application
+    app.run()
