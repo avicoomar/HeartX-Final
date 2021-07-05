5a6d7359eeb8a68617e1c68c7fb6c801ad69f496
Avi Coomar <61863442+avicoomar@users.noreply.github.com>
2021-07-05 23:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-
+import joblib
 import numpy as np
 import pickle
 from flask import Flask, request, render_template
 
 # Load ML model
-model = pickle.load(open("testmodel.pkl", "rb"))
-
+#model = pickle.load(open("testmodel.pkl", "rb")).....This doesnt work apparently
+model = joblib.load(open(r."\testmodel.pkl",'rb'))
 # Create application
 app = Flask(__name__)
 #app = Flask('flaskapp', static_url_path='/avi_assets')
